a2ebeae945b797b42adb06536328be924f79f967
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2022-12-12 20:19:39 -0300

    alerts
    alerts

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import ExternalServices from './ExternalServices';
 import ProductList from './productList';
 import { loadHeaderFooter } from './utils.js';
+import Alert from './alert';
 
 loadHeaderFooter();
 
@@ -11,3 +12,8 @@ const listElement = document.querySelector('.product-list');
 const myList = new ProductList('tents', myData, listElement);
 
 myList.init();
+
+
+const alert = new Alert();
+
+alert.createAlert();
