f28221f7d6f85aecd24bc769ab4656a2e2fdcd99
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2022-12-12 20:54:06 -0300

    working on the remove items

--- build/js/product.js
+++ build/js/product.js
@@ -9,6 +9,11 @@ function setLocalStorage(t, e) {
   localStorage.setItem(t, JSON.stringify(e))
 }
 
+function removeLocalStorage(t, e) {
+  localStorage.removeItem(t, JSON.stringify(e))
+}
+
+
 function getProductsData() {
   fetch("../json/tents.json").then(convertToJson).then(t => {
     products = t
@@ -19,4 +24,13 @@ function addToCart(t) {
   const e = products.find(n => n.Id === t.target.dataset.id);
   setLocalStorage("so-cart", e)
 }
+
+function removeFromCart(t) {
+  const e = products.find(n => n.Id === t.target.dataset.id);
+  removeLocalStorage("so-cart", e)
+}
+
+
 getProductsData(), document.getElementById("addToCart").addEventListener("click", addToCart);
+
+getProductsData(), document.getElementById("cart-card__remove").addEventListener("click", removeFromCart);
